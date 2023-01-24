10fc4aed2dc9dba87bbc74c092161398a78a819c
AleksandraMN <al_mn@mail.ru>
2023-01-24 17:37:27 +1000

    JS-5

---
+++ homework4.js
@@ -0,0 +1,72 @@
+/*
+1.	Необходимо вывести в консоль массив продуктов в котором есть хоть одна фотография используя метод filter. Исходные данные - массив products.
+2.	Необходимо отсортировать массив products используя метод sort по цене, начиная с самой маленькой, заканчивая самой большой ценой, после чего вывести отсортированный массив в консоль.
+const products = [
+    {
+      id: 3,
+      price: 127,
+      photos: [
+        "1.jpg",
+        "2.jpg",
+      ],
+    },
+    {
+      id: 5,
+      price: 499,
+      photos: [],
+    },
+    {
+      id: 10,
+      price: 26,
+      photos: [
+        "3.jpg",
+      ],
+    },
+    {
+      id: 8,
+      price: 78,
+    },
+  ];
+*/
+
+const products2 = [
+  {
+    id: 3,
+    price: 127,
+    photos: [
+      "1.jpg",
+      "2.jpg",
+    ],
+  },
+  {
+    id: 5,
+    price: 499,
+    photos: [],
+  },
+  {
+    id: 10,
+    price: 26,
+    photos: [
+      "3.jpg",
+    ],
+  },
+  {
+    id: 8,
+    price: 78,
+  },
+];
+
+//Необходимо вывести в консоль массив продуктов, в котором есть хоть одна фотография, используя метод filter. Исходные данные - массив products.
+
+const filteredProducts = products2.filter((products2) => products2.photos?.length); // первый способ - опциональная цепочка
+//const filteredProducts = products2.filter((product) => product.photos > []; // второй способ
+
+console.log('filteredProducts =', filteredProducts);
+
+
+//Необходимо отсортировать массив products, используя метод sort по цене, начиная с самой маленькой, заканчивая самой большой ценой, после чего вывести отсортированный массив в консоль.
+
+
+const sortedProducts = products2.sort((a, b) => a.price - b.price);
+console.log('sortedProducts =', sortedProducts);
+
